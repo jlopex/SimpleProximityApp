89dfe0730451bf1aa4638e4941c089325028f6ac
Javier Lopez <jlopex@cozybit.com>
2013-07-09 00:39:58 -0700

    Use Wifi Direct as network type

--- src/com/cozybit/simple/TestActivity.java
+++ src/com/cozybit/simple/TestActivity.java
@@ -3,7 +3,10 @@ package com.cozybit.simple;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.view.Menu;
@@ -15,14 +18,17 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-// Proximity imports
-
+import com.cozybit.proximity.ProximityAuth;
+import com.cozybit.proximity.ProximityIntent;
+import com.cozybit.proximity.ProximityInterface;
 import com.cozybit.proximity.ProximityManager;
 import com.cozybit.proximity.ProximityNetwork;
+import com.cozybit.proximity.ProximityProfile;
 import com.cozybit.proximity.mint.IMintChannelListener;
 import com.cozybit.proximity.mint.IMintListener;
 import com.cozybit.proximity.mint.Mint;
 import com.cozybit.proximity.mint.MintChannel;
+// Proximity imports
 
 public class TestActivity extends Activity {
 
@@ -111,6 +117,18 @@ public class TestActivity extends Activity {
 		
 		@Override
 		public void onError(int reason) {}
+	};
+
+	private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			
+			if (intent.getAction().equals(ProximityIntent.PROXIMITY_NETWORK_CONNECTED)) {
+				Toast.makeText(context, "Got proximity network with Interface " +
+			    intent.getStringExtra(ProximityIntent.EXTRA_PROXIMITY_INTERFACE_NAME), Toast.LENGTH_SHORT).show();
+			}
+		}
 	};
 
 	// UI stuff...
@@ -205,7 +223,17 @@ public class TestActivity extends Activity {
 		if (mManager != null) {
 			Toast.makeText(mContext, "Getting ProximityNetwork", Toast.LENGTH_SHORT).show();
 			try {
-				mProximityNetwork = mManager.getProximityNetwork();
+				// WFD
+				ProximityAuth auth = ProximityAuth.PLAIN;
+				mProximityNetwork = mManager.getProximityNetwork(
+						ProximityInterface.WIFI_DIRECT,
+						auth,
+						ProximityProfile.DEFAULT);
+
+				IntentFilter filter = new IntentFilter(ProximityIntent.PROXIMITY_NETWORK_CONNECTED);
+				filter.addAction(ProximityIntent.PROXIMITY_NETWORK_DISCONNECTED);
+				filter.addAction(ProximityIntent.PROXIMITY_NETWORK_FAILED);
+				mProximityNetwork.registerReceiver(mReceiver, filter);
 			} catch (IllegalStateException e) {
 				Toast.makeText(mContext, "Error getting ProximityNetwork", Toast.LENGTH_SHORT).show();
 			} catch (RemoteException e) {
